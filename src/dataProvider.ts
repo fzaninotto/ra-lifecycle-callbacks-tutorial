e35031dd0413a37ba916214347b5aca5c57bc73b
François Zaninotto <fzaninotto@gmail.com>
2024-01-27 08:56:33 +0100

    Refactor lifecycle callbacks for less repetition

--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -1,29 +1,40 @@
 import fakeRestProvider from "ra-data-fakerest";
-import { withLifecycleCallbacks } from "react-admin";
+import { DataProvider, Identifier, withLifecycleCallbacks } from "react-admin";
 import { data } from "./data";
 
 let productsToUpdate: string[] = [];
+
+const updateProductAverageNote = async (productId: Identifier, dataProvider: DataProvider) => {
+  const { data, total } = await dataProvider.getList("reviews", {
+    filter: { product_id: productId },
+    pagination: { page: 1, perPage: 100 },
+    sort: { field: "date", order: "DESC" },
+  });
+  const averageNote = data.reduce(
+    (acc: number, review: any) => acc + review.rating,
+    0
+  ) / total!;
+  await dataProvider.update("products", {
+    id: productId,
+    data: { average_note: averageNote  },
+    previousData: {},
+  });
+}
 
 export const dataProvider = withLifecycleCallbacks(
   fakeRestProvider(data, true),
   [
     {
+      resource: 'reviews',
+      afterSave: async (data: any, dataProvider) => {
+        await updateProductAverageNote(data.product_id, dataProvider);
+        return data;
+      }
+    },
+    {
       resource: "reviews",
       afterDelete: async (result: any, dataProvider) => {
-        // update the product average note
-        const { data: reviews } = await dataProvider.getList("reviews", {
-          filter: { product_id: result.data.product_id },
-          pagination: { page: 1, perPage: 100 },
-          sort: { field: "date", order: "DESC" },
-        });
-        const average_note =
-          reviews.reduce((avg: number, review: any) => avg + review.rating, 0) /
-          reviews.length;
-        await dataProvider.update("products", {
-          id: result.data.product_id,
-          data: { average_note },
-          previousData: undefined,
-        });
+        await updateProductAverageNote(result.data.product_id, dataProvider);
         return result;
       },
     },
@@ -48,21 +59,8 @@ export const dataProvider = withLifecycleCallbacks(
       resource: "reviews",
       afterDeleteMany: async (result: any, dataProvider) => {
         // Update products average note
-        for (const product of productsToUpdate) {
-          const { data } = await dataProvider.getList("reviews", {
-            filter: { product_id: product },
-            pagination: { page: 1, perPage: 100 },
-            sort: { field: "id", order: "ASC" },
-          });
-          const averageNote = data.reduce(
-            (acc: number, review: any) => acc + review.rating,
-            0
-          );
-          await dataProvider.update("products", {
-            id: product,
-            data: { average_note: averageNote / data.length },
-            previousData: {},
-          });
+        for (const productId of productsToUpdate) {
+          await updateProductAverageNote(productId, dataProvider);
         }
         return result;
       },
